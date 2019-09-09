20f36b3aeba1c922f7e0b878df8d112339d376be
Mario Macias <mmacias@newrelic.com>
2019-09-09 14:59:58 +0200

    Restructured metric names

---
+++ src/docker/metric.go
@@ -0,0 +1,41 @@
+package docker
+
+import "github.com/newrelic/infra-integrations-sdk/data/metric"
+
+const ContainerSampleName = "DockerContainerSample"
+
+func metricFunc(name string, sType metric.SourceType) func(interface{}) Metric {
+	return func(value interface{}) Metric {
+		return Metric{Name: name, Type: sType, Value: value}
+	}
+}
+
+var (
+	MetricCommandLine             = metricFunc("commandLine", metric.ATTRIBUTE)
+	MetricUser                    = metricFunc("user", metric.ATTRIBUTE)
+	MetricContainerImage          = metricFunc("image", metric.ATTRIBUTE)
+	MetricContainerImageName      = metricFunc("imageName", metric.ATTRIBUTE)
+	MetricContainerName           = metricFunc("name", metric.ATTRIBUTE)
+	MetricContainerID             = metricFunc("id", metric.ATTRIBUTE)
+	MetricState                   = metricFunc("state", metric.ATTRIBUTE)
+	MetricCPUPercent              = metricFunc("cpuPercent", metric.GAUGE)
+	MetricCPUSystemPercent        = metricFunc("cpuSystemPercent", metric.GAUGE)
+	MetricCPUUserPercent          = metricFunc("cpuUserPercent", metric.GAUGE)
+	MetricMemoryVirtualSizeBytes  = metricFunc("memoryVirtualSizeBytes", metric.GAUGE)
+	MetricMemoryResidentSizeBytes = metricFunc("memoryResidentSizeBytes", metric.GAUGE)
+	MetricIOReadCountPerSecond    = metricFunc("ioReadCountPerSecond", metric.GAUGE)
+	MetricIOWriteCountPerSecond   = metricFunc("ioWriteCountPerSecond", metric.GAUGE)
+	MetricIOReadBytesPerSecond    = metricFunc("ioReadBytesPerSecond", metric.GAUGE)
+	MetricIOWriteBytesPerSecond   = metricFunc("ioWriteBytesPerSecond", metric.GAUGE)
+	MetricIOTotalReadCount        = metricFunc("ioTotalReadCount", metric.GAUGE)
+	MetricIOTotalWriteCount       = metricFunc("ioTotalWriteCount", metric.GAUGE)
+	MetricIOTotalReadBytes        = metricFunc("ioTotalReadBytes", metric.GAUGE)
+	MetricIOTotalWriteBytes       = metricFunc("ioTotalWriteBytes", metric.GAUGE)
+	MetricPIDs                    = metricFunc("pidsNumber", metric.GAUGE)
+)
+
+type Metric struct {
+	Name  string
+	Type  metric.SourceType
+	Value interface{}
+}
