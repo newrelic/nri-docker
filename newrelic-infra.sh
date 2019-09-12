37a4b3bd80385c060b15cd2fc68e19bb033d823e
David Brota <dbrota@newrelic.com>
2019-09-12 09:32:23 +0200

    dockerfile

--- newrelic-infra.sh
+++ newrelic-infra.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
+
 if [ ! -z "$NRIA_MONITOR_DOCKER" ]; then
     # enable nri-docker integration
     cp /etc/newrelic-infra/integrations.d/docker-config.yml.sample /etc/newrelic-infra/integrations.d/docker-config.yml
 fi
 
-/bin/local/newrelic-infra
+/usr/bin/newrelic-infra
