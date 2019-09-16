18cfa4e69e5a740a2690defbd4add65999e35ae8
Mario Macias <mmacias@newrelic.com>
2019-09-16 14:51:17 +0200

    calculating memory from cgroups

--- src/stats/cgroup.go
+++ src/stats/cgroup.go
@@ -92,7 +92,7 @@ func (cg *CGroupsProvider) Fetch(containerID string) (Cooked, error) {
 		log.Error("couldn't read cpu stats: %v", err)
 	}
 
-	if err := readMemoryStats(containerID, &stats.MemoryStats); err != nil {
+	if err := memoryStats(metrics, &stats.MemoryStats); err != nil {
 		log.Error("couldn't read memory stats: %v", err)
 	}
 
@@ -268,60 +268,31 @@ func cpuStats(metric *cgroups.Metrics, stats *types.CPUStats) error {
 	return nil
 }
 
-// TODO: use cgroups library (as for readPidStats)
-func readMemoryStats(containerID string, stats *types.MemoryStats) error {
-	cpath := hostFolder(cgroupPath, "memory", "docker", containerID)
+func memoryStats(metric *cgroups.Metrics, stats *types.MemoryStats) error {
 
-	for _, metric := range []struct {
-		file string
-		dest *uint64
-	}{
-		{"memory.max_usage_in_bytes", &stats.MaxUsage},
-		{"memory.limit_in_bytes", &stats.Limit},
-	} {
-		var err error
-		if *metric.dest, err = parseUintFile(path.Join(cpath, metric.file)); err != nil {
-			log.Debug("error reading %s: %s", metric.file, err.Error())
-		}
+	stats.MaxUsage = metric.Memory.Usage.Max
+	stats.Limit = metric.Memory.Usage.Limit
+	stats.Stats = map[string]uint64{
+		"cache": metric.Memory.Cache,
+		"rss":   metric.Memory.RSS,
 	}
 
-	stats.Stats = map[string]uint64{}
-
-	f, err := os.Open(path.Join(cpath, "memory.stat"))
-	if err != nil {
-		log.Debug("error reading memory.stat: %s", err.Error())
-	}
-
-	defer f.Close()
-	sc := bufio.NewScanner(f)
-	for sc.Scan() {
-		fields := strings.Split(sc.Text(), " ")
-		if len(fields) < 2 {
-			log.Debug("%q has less than two fields", sc.Text())
-			continue
-		}
-		value, err := strconv.ParseUint(fields[1], 10, 64)
-		if err != nil {
-			log.Debug("error parsing memory.stat %q: %s", fields[0], err.Error())
-			continue
-		}
-
-		stats.Stats[fields[0]] = value
-	}
-
-	/*
-		Calculating usage instead of `memory.usage_in_bytes` file contents.
-		https://www.kernel.org/doc/Documentation/cgroup-v1/memory.txt
-		For efficiency, as other kernel components, memory cgroup uses some optimization
-		to avoid unnecessary cacheline false sharing. usage_in_bytes is affected by the
-		method and doesn't show 'exact' value of memory (and swap) usage, it's a fuzz
-		value for efficient access. (Of course, when necessary, it's synchronized.)
-		If you want to know more exact memory usage, you should use RSS+CACHE(+SWAP)
-		value in memory.stat(see 5.2).
-		However, as the `docker stats` cli tool does, page cache is intentionally
-		excluded to avoid misinterpretation of the output.
+	/* Calculating usage instead of `memory.usage_in_bytes` file contents.
+	https://www.kernel.org/doc/Documentation/cgroup-v1/memory.txt
+	For efficiency, as other kernel components, memory cgroup uses some optimization
+	to avoid unnecessary cacheline false sharing. usage_in_bytes is affected by the
+	method and doesn't show 'exact' value of memory (and swap) usage, it's a fuzz
+	value for efficient access. (Of course, when necessary, it's synchronized.)
+	If you want to know more exact memory usage, you should use RSS+CACHE(+SWAP)
+	value in memory.stat(see 5.2).
+	However, as the `docker stats` cli tool does, page cache is intentionally
+	excluded to avoid misinterpretation of the output.
+	Also the Swap usage is parsed from memory.memsw.usage_in_bytes, which
+	according to the documentation reports the sum of current memory usage
+	plus swap space used by processes in the cgroup (in bytes). That's why
+	Usage is subtracted from the Swap: to get the actual swap.
 	*/
-	stats.Usage = stats.Stats["rss"] + stats.Stats["swap"]
+	stats.Usage = metric.Memory.RSS + metric.Memory.Swap.Usage - metric.Memory.Usage.Usage
 
 	return nil
 }
