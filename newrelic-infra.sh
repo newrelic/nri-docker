da1d16e8d1bcdf16cebf3af0a2f709335fdbf280
David Brota <dbrota@newrelic.com>
2019-09-19 16:17:35 +0200

    fix script condition

--- newrelic-infra.sh
+++ newrelic-infra.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ "$NRIA_DOCKER_ENABLED" = "yes" ]; then
+if [ "$NRIA_DOCKER_ENABLED" = "true" ]; then
     # enable nri-docker integration
     cp /etc/newrelic-infra/integrations.d/docker-config.yml.sample /etc/newrelic-infra/integrations.d/docker-config.yml
 fi
