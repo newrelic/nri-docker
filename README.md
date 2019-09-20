2ef23867da8a4296e391caff98146b31f6d59d14
Mario Macias <mmacias@newrelic.com>
2019-09-20 10:55:05 +0200

    Memory testing

--- README.md
+++ README.md
@@ -5,13 +5,22 @@ Reports status and metrics of Docker containers running into a host.
 ## Requirements
 
 * Linux (Windows support TBD)
-* New Relic Infrastructure Agent > 1.5.XX (version to be released)
+* New Relic Infrastructure Agent **1.5.42**
+    - ⚠️ prior versions won't provide the data that is required for
+      full functionality in the New Relic Site.
 * Docker
 
-## Configuration
+## Running as a container
 
-TBD
+You need to put your New Relic Infrastructure license key as a value for
+the `NRIA_LICENSE_KEY` property.
 
-## Installation
+```
+docker run -d --name newrelic-infra --network=host --cap-add=SYS_PTRACE \
+-v "/:/host:ro" -v "/var/run/docker.sock:/var/run/docker.sock" \
+-e NRIA_MONITOR_DOCKER="true" -e NRIA_CONNECT_ENABLED="true" \
+-e NRIA_LICENSE_KEY="<your new relic license key>" \
+xxxx:xxxx
+```
 
-TBD
+`xxxx:xxxx` -> image still to be published 
--- src/biz/metrics_test.go
+++ src/biz/metrics_test.go
@@ -18,16 +18,18 @@ import (
 )
 
 const (
+	eventuallyTimeout       = time.Minute
 	testDockerClientVersion = "1.24"
 	imageTag                = "stress:latest"
 	containerName           = "nri_docker_test"
 	cpus                    = 0.5
-	mem                     = 1e8 // 100 MB of memory
+	memLimitStr             = "100M"
+	memLimit                = 100 * 1024 * 1024 // 100 MB of memory
 )
 
 func TestHighCPU(t *testing.T) {
 	// GIVEN a container consuming a lot of CPU
-	containerID, dockerRM := stress(t, "-c", "2", "-l 100", "-t", "5m")
+	containerID, dockerRM := stress(t, "stress-ng", "-c", "4", "-l 100", "-t", "5m")
 	defer dockerRM()
 
 	// WHEN its metrics are sampled and processed
@@ -42,9 +44,9 @@ func TestHighCPU(t *testing.T) {
 
 	// THEN the CPU static metrics belong to the container
 	assert.InDelta(t, cpus, sample.CPU.LimitCores, 0.01)
-	assert.True(t, sample.Pids.Current > 0, "pids can't be 0")
+	assert.True(t, sample.Pids.Current >= 4, "pids need to be >= 4") // because we invoked stress-ng -c 4
 
-	test.Eventually(t, 15*time.Second, func(t require.TestingT) {
+	test.Eventually(t, eventuallyTimeout, func(t require.TestingT) {
 		time.Sleep(100 * time.Millisecond)
 		sample, err := metrics.Process(containerID)
 		require.NoError(t, err)
@@ -52,9 +54,9 @@ func TestHighCPU(t *testing.T) {
 		cpu := sample.CPU
 
 		// AND the samples tend to show CPU metrics near to their limits
-		assert.InDelta(t, 100*cpus, cpu.CPUPercent, 10)
-		assert.InDelta(t, 100, cpu.UsedCoresPercent, 10)
-		assert.InDelta(t, cpus, cpu.UsedCores, 0.1)
+		assert.InDelta(t, 100*cpus, cpu.CPUPercent, 15)
+		assert.InDelta(t, 100, cpu.UsedCoresPercent, 15)
+		assert.InDelta(t, cpus, cpu.UsedCores, 0.2)
 
 		assert.True(t, cpu.UserPercent > 0,
 			"user percent not > 0")
@@ -69,7 +71,7 @@ func TestHighCPU(t *testing.T) {
 
 func TestLowCPU(t *testing.T) {
 	// GIVEN a container consuming almost no CPU
-	containerID, dockerRM := stress(t, "-c", "0", "-l", "0")
+	containerID, dockerRM := stress(t, "stress-ng", "-c", "0", "-l", "0", "-t", "5m")
 	defer dockerRM()
 
 	// WHEN its metrics are sampled and processed
@@ -86,7 +88,7 @@ func TestLowCPU(t *testing.T) {
 	assert.InDelta(t, cpus, sample.CPU.LimitCores, 0.01)
 	assert.True(t, sample.Pids.Current > 0, "pids can't be 0")
 
-	test.Eventually(t, 15*time.Second, func(t require.TestingT) {
+	test.Eventually(t, eventuallyTimeout, func(t require.TestingT) {
 		time.Sleep(100 * time.Millisecond)
 		sample, err := metrics.Process(containerID)
 		require.NoError(t, err)
@@ -94,9 +96,39 @@ func TestLowCPU(t *testing.T) {
 		cpu := sample.CPU
 
 		// AND the samples tend to show CPU metrics near zero
-		assert.InDelta(t, 0, cpu.CPUPercent, 5)
-		assert.InDelta(t, 0, cpu.UsedCoresPercent, 5)
+		assert.InDelta(t, 0, cpu.CPUPercent, 10)
+		assert.InDelta(t, 0, cpu.UsedCoresPercent, 10)
 		assert.InDelta(t, 0, cpu.UsedCores, 0.1)
+	})
+}
+
+func TestMemory(t *testing.T) {
+	// GIVEN a container with a process consuming 60M of its limit of 100M
+	containerID, dockerRM := stress(t, "stress-ng", "-c", "0", "-l", "0", "--vm", "1", "--vm-bytes", "60M", "-t", "5m")
+	defer dockerRM()
+
+	// WHEN its metrics are sampled and processed
+	docker := newDocker(t)
+	defer docker.Close()
+	metrics := NewProcessor(
+		persist.NewInMemoryStore(),
+		raw.NewFetcher("/"),
+		docker)
+	// Then the Memory metrics are reported according to the usage and limits
+	test.Eventually(t, eventuallyTimeout, func(t require.TestingT) {
+		sample, err := metrics.Process(containerID)
+		require.NoError(t, err)
+
+		mem := sample.Memory
+		const expectedUsage = 60 * 1024 * 1024 // 60MB
+		assert.Truef(t, mem.UsageBytes >= expectedUsage,
+			"reported usage %v should be >= 60MB (%v)", mem.UsageBytes, expectedUsage)
+		assert.Truef(t, mem.RSSUsageBytes >= expectedUsage,
+			"reported RSS %v should be >= 60MB (%v)", mem.RSSUsageBytes, expectedUsage)
+		// todo: test cachebytes against a fixed value
+		assert.True(t, mem.CacheUsageBytes > 0, "reported cache bytes %v should not be zero")
+
+		assert.EqualValues(t, memLimit, mem.MemLimitBytes)
 	})
 }
 
@@ -104,22 +136,19 @@ func newDocker(t *testing.T) *client.Client {
 	t.Helper()
 	docker, err := client.NewEnvClient()
 	require.NoError(t, err)
-	docker.UpdateClientVersion(testDockerClientVersion) // TODO: make it configurable
+	docker.UpdateClientVersion(testDockerClientVersion)
 	return docker
-}
-
-func buildTestImage(t *testing.T) {
-	t.Helper()
-
-	cmd := exec.Command("docker", "build", "-t", imageTag, ".")
-	require.NoError(t, cmd.Run())
-	log.Println(cmd.CombinedOutput())
 }
 
 func stress(t *testing.T, args ...string) (containerID string, closeFunc func()) {
 	t.Helper()
 
-	arguments := []string{"run", "-d", "--name", containerName, "--rm", "--cpus", fmt.Sprint(cpus), imageTag}
+	arguments := []string{
+		"run", "-d", "--rm",
+		"--name", containerName,
+		"--cpus", fmt.Sprint(cpus),
+		"--memory", memLimitStr,
+		imageTag}
 	arguments = append(arguments, args...)
 	cmd := exec.Command("docker", arguments...)
 	stdout := bytes.Buffer{}
