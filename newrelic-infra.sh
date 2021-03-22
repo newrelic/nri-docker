ea5346068fbd47269ef4cf563fccc74aed7e51d9
David Brota <davidbrota@gmail.com>
2021-03-22 09:57:43 +0100

    removed newrelic-infra.sh script as it's no longer in use

--- newrelic-infra.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ "$NRIA_DOCKER_ENABLED" = "true" ]; then
-    # enable nri-docker integration
-    cp /etc/newrelic-infra/integrations.d/docker-config.yml.sample /etc/newrelic-infra/integrations.d/docker-config.yml
-fi
-
-tini -- /usr/bin/newrelic-infra
