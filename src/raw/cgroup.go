35505d25373e949e379a52615ad58b0e56cbb63c
Marc Sanmiquel <marcsanmiquel@gmail.com>
2022-05-03 16:00:36 +0200

    Adapt cgroups fetcher to support cgroupsV2 (#98)
    * feat: update cgroups fetcher builder to include cgroups info
    
    * feat: add fetcher for containerd cgroupsv2
    
    * refactor: update cgroupsFetcherV2 with cgroupsV2Paths
    
    * refactor: add cpu shares metric
    
    * fix: add fetcher cgroup drive

--- src/raw/cgroup.go
+++ src/raw/cgroup.go
@@ -2,6 +2,9 @@ package raw
 
 // NewCgroupsFetcher creates the proper metrics fetcher for the used cgroups version.
 func NewCgroupsFetcher(hostRoot string, cgroupInfo *CgroupInfo, systemCPUReader SystemCPUReader) (Fetcher, error) {
-	// TODO: use cgroup version from cgroupInfo and create the corresponding fetcher.
+	if cgroupInfo.Version == CgroupV2 {
+		return NewCgroupsV2Fetcher(hostRoot, systemCPUReader, cgroupInfo.Driver)
+	}
+
 	return NewCgroupsV1Fetcher(hostRoot, systemCPUReader)
 }
