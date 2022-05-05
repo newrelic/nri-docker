2eca81a5316139a2d998678e24126438a5e378b6
Christian Felipe Álvarez <calvarez@newrelic.com>
2022-05-05 17:19:16 +0200

    feat: include report OnlineCPUs raw metric for cgroups2

--- src/raw/cgroupv2_detect.go
+++ src/raw/cgroupv2_detect.go
@@ -21,6 +21,10 @@ type cgroupV2Paths struct {
 	Group string
 }
 
+func (c *cgroupV2Paths) FullPath() string {
+	return filepath.Join(c.MountPoint, c.Group)
+}
+
 func getSingleFileUintStat(cGroupV2Paths *cgroupV2Paths, stat string) (uint64, error) {
 	// get full path
 	// /sys/fs/cgroup/system.slice/cpu.weight
