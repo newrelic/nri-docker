9936ddaf2380493df944f67f91fa5809477e45c7
Christian Felipe Álvarez <calvarez@newrelic.com>
2024-02-07 09:35:56 +0100

    feat: introduce dockerapi fetcher scaffolding

---
+++ test/integration/dockerapi_fetcher_test.go
@@ -0,0 +1,39 @@
+package integration_test
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+
+	"github.com/docker/docker/client"
+	"github.com/newrelic/nri-docker/src/raw/dockerapi"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDockerAPIHelpers(t *testing.T) {
+	// Build the client and the fetcher
+	// The API Version can be set up using `args.DockerClientVersion` (defaults to 1.24 for now)
+	dockerClient, err := client.NewClientWithOpts(client.FromEnv, client.WithVersion("1.24"))
+	require.NoError(t, err)
+	f := dockerapi.NewFetcher(dockerClient)
+
+	// run a container for testing purposes
+	containerID, dockerRM := stress(t, "stress-ng", "-c", "0", "-l", "0", "-t", "5m")
+	defer dockerRM()
+
+	// show container inspect data (it's done in biz/metrics)
+	inspectData, err := dockerClient.ContainerInspect(context.Background(), containerID)
+	require.NoError(t, err)
+	logAsJSON(t, "Inspect data", &inspectData)
+
+	// fetch stats data
+	statsData, err := f.ContainerStats(context.Background(), containerID)
+	require.NoError(t, err)
+	logAsJSON(t, "Container Stats", &statsData)
+}
+
+func logAsJSON(t *testing.T, title string, data any) {
+	b, err := json.Marshal(data)
+	require.NoError(t, err)
+	t.Logf("%s: %s", title, string(b))
+}
