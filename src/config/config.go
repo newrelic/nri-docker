613ce098e1109dafee8f1e24488f89a09e433ad1
Paolo Gallina <paologallina1992@gmail.com>
2024-02-14 11:52:09 +0100

    feat(flag): added new flag for dockerAPI

--- src/config/config.go
+++ src/config/config.go
@@ -10,6 +10,7 @@ type ArgumentList struct {
 	DockerClientVersion   string `default:"1.24" help:"Optional. Specify the version of the docker client. Used for compatibility."`
 	DisableStorageMetrics bool   `default:"false" help:"Disables storage driver metrics collection."`
 	ShowVersion           bool   `default:"false" help:"Print build information and exit"`
+	UseDockerAPI          bool   `default:"false" help:"Leverage Docker API to gather metrics instead of filesystem. This option is ignored if cgroupsV1 are detected"`
 	// CgroupPath and CgroupDriver arguments are not used but are kept here for backwards compatibility reasons.
 	CgroupPath   string `default:"" help:"Deprecated. cgroup_path argument is not used anymore."`
 	CgroupDriver string `default:"" help:"Deprecated. cgroup_driver argument is not used anymore."`
--- src/docker.go
+++ src/docker.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/newrelic/nri-docker/src/raw/dockerapi"
 	"os"
 	"runtime"
 
@@ -72,12 +73,28 @@ func populateFromDocker(i *integration.Integration, args config.ArgumentList) {
 	cgroupInfo, err := dockerClient.Info(context.Background())
 	exitOnErr(err)
 
-	fetcher, err := raw.NewCgroupFetcher(args.HostRoot, cgroupInfo)
-	exitOnErr(err)
+	var fetcher raw.Fetcher
+	if useDockerAPI(args, cgroupInfo.CgroupVersion) {
+		fetcher = dockerapi.NewFetcher(dockerClient)
+	} else { // use cgroups as source of data
+		fetcher, err = raw.NewCgroupFetcher(args.HostRoot, cgroupInfo)
+		exitOnErr(err)
+	}
 
 	sampler, err := nri.NewSampler(fetcher, dockerClient, args)
 	exitOnErr(err)
 	exitOnErr(sampler.SampleAll(context.Background(), i, cgroupInfo))
+}
+
+func useDockerAPI(args config.ArgumentList, version string) bool {
+	if args.UseDockerAPI {
+		if version == raw.CgroupV2 {
+			return true
+		}
+		log.Debug("UseDockerAPI config is not available on CgroupV1")
+		return false
+	}
+	return false
 }
 
 func exitOnErr(err error) {
