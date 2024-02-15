c718c7bc4cc3e94af55987cba1c6d5d31ec2d94c
Christian <calvarez@newrelic.com>
2024-02-15 09:04:54 +0100

    NR-221185: add docker api cpu metrics (#214)

--- src/raw/dockerapi/fetcher_test.go
+++ src/raw/dockerapi/fetcher_test.go
@@ -8,6 +8,8 @@ import (
 	"testing"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/newrelic/nri-docker/src/raw"
 	"github.com/newrelic/nri-docker/src/raw/dockerapi"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -33,6 +35,22 @@ var mockStats = types.StatsJSON{
 		"eth0": mockNetworkStats,
 		"eth1": mockNetworkStats,
 		"eth2": mockNetworkStats,
+	},
+	Stats: types.Stats{
+		CPUStats: types.CPUStats{
+			CPUUsage: types.CPUUsage{
+				TotalUsage:        11491000,
+				UsageInKernelmode: 5745000,
+				UsageInUsermode:   5745000,
+			},
+			ThrottlingData: types.ThrottlingData{
+				Periods:          2,
+				ThrottledPeriods: 1,
+				ThrottledTime:    20000,
+			},
+			SystemUsage: 31532890000000,
+			OnlineCPUs:  2,
+		},
 	},
 }
 
@@ -68,4 +86,39 @@ func Test_NetworkMetrics(t *testing.T) {
 	assert.Equal(t, expectedValue, metrics.Network.TxDropped)
 	assert.Equal(t, expectedValue, metrics.Network.TxErrors)
 	assert.Equal(t, expectedValue, metrics.Network.TxPackets)
+}
+
+func Test_CPUMetrics(t *testing.T) {
+	client := mockDockerStatsClient{}
+	client.On("ContainerStats", mock.Anything).Return(mockStats)
+
+	fetcher := dockerapi.NewFetcher(&client)
+
+	expectedCPUMetrics := raw.CPU{
+		TotalUsage:        11491000,
+		UsageInKernelmode: 5745000,
+		UsageInUsermode:   5745000,
+		PercpuUsage:       nil,
+		Shares:            2048,
+		ThrottledPeriods:  1,
+		ThrottledTimeNS:   20000,
+		SystemUsage:       31532890000000,
+		OnlineCPUs:        2,
+	}
+
+	container := types.ContainerJSON{
+		ContainerJSONBase: &types.ContainerJSONBase{
+			ID:         "test",
+			HostConfig: &container.HostConfig{Resources: container.Resources{CPUShares: 2048}},
+		},
+	}
+	metrics, err := fetcher.Fetch(container)
+	require.NoError(t, err)
+
+	assert.Equal(t, expectedCPUMetrics, metrics.CPU)
+
+	containerWithNoHostConfig := types.ContainerJSON{ContainerJSONBase: &types.ContainerJSONBase{ID: "test"}}
+	metricsNoHostConfig, err := fetcher.Fetch(containerWithNoHostConfig)
+	require.NoError(t, err)
+	assert.EqualValues(t, 0, metricsNoHostConfig.CPU.Shares, "When hostConfig is not available, cpu shares cannot be set")
 }
