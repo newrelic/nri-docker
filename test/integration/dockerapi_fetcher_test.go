b4075cdb1f3e365d0da9c2217fd0307431674414
Christian Felipe Álvarez <calvarez@newrelic.com>
2024-02-16 14:27:59 +0100

    feat: extend docker-api integration test

--- test/integration/dockerapi_fetcher_test.go
+++ test/integration/dockerapi_fetcher_test.go
@@ -10,7 +10,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// It currently it shows how to get container stats data and can be useful for development purposes.
 func TestDockerAPIFetcher(t *testing.T) {
 	dockerClient := newDocker(t)
 
@@ -23,24 +22,49 @@ func TestDockerAPIFetcher(t *testing.T) {
 	fetcher := dockerapi.NewFetcher(dockerClient)
 
 	// run a container for testing purposes
-	containerID, dockerRM := stress(t, "stress-ng", "-c", "0", "-l", "0", "-t", "5m")
+	containerID, dockerRM := stress(t, "stress-ng", "-c", "2", "-l", "50", "-t", "5m", "--iomix", "10")
 	defer dockerRM()
 
-	// show container inspect data (it's done in biz/metrics)
 	inspectData, err := dockerClient.ContainerInspect(context.Background(), containerID)
 	require.NoError(t, err)
 
-	assert.EventuallyWithT(t, func(t *assert.CollectT) {
+	assert.EventuallyWithT(t, func(ct *assert.CollectT) {
 		statsData, err := fetcher.Fetch(inspectData)
-		require.NoError(t, err)
+		require.NoError(ct, err)
+
+		// CPU metrics
+		assert.NotZero(ct, statsData.CPU.TotalUsage)
+		assert.NotZero(ct, statsData.CPU.UsageInUsermode)
+		assert.NotZero(ct, statsData.CPU.UsageInKernelmode)
+		assert.NotZero(ct, statsData.CPU.SystemUsage)
+		assert.NotZero(ct, statsData.CPU.ThrottledPeriods)
+		assert.NotZero(ct, statsData.CPU.ThrottledTimeNS)
+		assert.NotZero(ct, statsData.CPU.OnlineCPUs)
+		assert.EqualValues(ct, cpuShares, statsData.CPU.Shares, "cpu-shares has been set, so it should be reported")
+
+		// Memory metrics
+		// SwapUsage cannot be fetched through docker API
+		assert.EqualValues(ct, statsData.Memory.UsageLimit, memLimit, "the memory limit has been set, so it should be reported")
+		assert.EqualValues(ct, statsData.Memory.SoftLimit, memReservation, "the memory soft limit has been set, so it should be reported")
+		assert.NotZero(ct, statsData.Memory.SwapLimit)
+		assert.NotZero(ct, statsData.Memory.Cache)
+		assert.NotZero(ct, statsData.Memory.RSS)
+		assert.NotZero(ct, statsData.Memory.KernelMemoryUsage)
 
 		// Network metrics
 		// Only RxBytes and RxPackets are generated
-		assert.NotZero(t, statsData.Network.RxBytes, "stress-ng should have generated RxBytes")
-		assert.NotZero(t, statsData.Network.RxPackets, "stress-ng should have generated RxPackets")
+		assert.NotZero(ct, statsData.Network.RxBytes, "stress-ng should have generated RxBytes")
+		assert.NotZero(ct, statsData.Network.RxPackets, "stress-ng should have generated RxPackets")
 
 		// Pids metrics
-		assert.Equal(t, pidsLimit, strconv.FormatUint(statsData.Pids.Limit, 10), "the limit has been set so it should be reported")
-		assert.NotZero(t, statsData.Pids.Current, "amount of processes or threads should be grater than 0")
+		assert.Equal(ct, pidsLimit, strconv.FormatUint(statsData.Pids.Limit, 10), "the limit has been set so it should be reported")
+		assert.NotZero(ct, statsData.Pids.Current, "amount of processes or threads should be grater than 0")
+		assert.NotZero(ct, statsData.Network.RxBytes)
+		assert.NotZero(ct, statsData.Network.RxPackets)
+
+		// Blkio metrics
+		// IoServicedRecursive can be empty if the io operations are not blocking
+		assert.NotEmpty(ct, statsData.Blkio.IoServiceBytesRecursive)
+
 	}, eventuallyTimeout, eventuallyTick)
 }
